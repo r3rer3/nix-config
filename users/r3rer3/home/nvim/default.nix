506f0f5262757bae398326ae2840583993dc37a8
r3rer3 <r3rer3@startmail.com>
2025-10-31 12:25:33 -0400

    update

--- users/r3rer3/home/nvim/default.nix
+++ users/r3rer3/home/nvim/default.nix
@@ -221,8 +221,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "yetone";
           repo = "avante.nvim";
-          rev = "164a86db6e5da78bc42310691a26b22d0763ceca";
-          hash = "sha256-hNKkb0PdKPC2m1a0stgCkAxk2P4uL8WiqiGpOY79Efo=";
+          rev = "7f48770e66684e9a7d4d5b9c47505a23e0167a6e";
+          hash = "sha256-U8RnTBGW+dR26JATQ10JbM8R03qX/ovPjvelHw2J7fc=";
         };
 
         nvimSkipModules = oldAttrs.nvimSkipModules ++ ["avante.providers.ollama" "avante.providers.vertex_claude" "avante.providers.vertex" "avante.providers.gemini"];
